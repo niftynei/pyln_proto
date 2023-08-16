677e9b76b08f43186e931eee292916209990c1ff
niftynei <niftynei@gmail.com>
2023-08-16 10:43:45 -0500

    add setup, flatten

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['proto']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pyln-proto',
+    'version': '23.5.2',
+    'description': 'Protocol Library for the Lightning Network',
+    'author': 'Christian Decker',
+    'author_email': 'decker.christian@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
